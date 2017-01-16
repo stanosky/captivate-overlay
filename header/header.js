a04596e951a3e63cc9c73820d536722fd630e490
stanosky <krzysztof.stano@gmail.com>
2017-01-16 15:49:47 +0100

    Working Header

--- header/header.js
+++ header/header.js
@@ -8,14 +8,14 @@
     var slideName = $('#slideName');
 
     $.getJSON("header/navigation.json", function(json) {
-        console.log('json',json);
+        //console.log('json',json);
         var navigation = json;
         var interfaceObj = evt.Data;
         var eventEmitterObj = interfaceObj.getEventEmitter();
         eventEmitterObj.addEventListener('CPAPI_SLIDEENTER',function(e){
-          console.log('navigation',navigation);
-          //if(navigation !== null) {
-            console.log(e.Data.slideNumber);
+          //console.log('navigation',navigation);
+          if(navigation !== null) {
+            //console.log(e.Data.slideNumber);
             var index = e.Data.slideNumber-1;
             var currSlide = navigation.slides[index];
             courseName.html(navigation.courseName);
@@ -23,8 +23,20 @@
             slideNumber.html(currSlide.index+'.');
             slideName.html(currSlide.label);
 
-          //}
+          }
         });
     });
+    $('#mnheader').slideUp(0);
+    $( "#mnrollover" )
+      .mouseenter(function(event) {
+        $('#mnheader').slideDown(100);
+        //var screenNumber = window.cpAPIInterface.getVariableValue('cpCmndGotoSlide');
+        //window.cpAPIInterface.setVariableValue('cpCmndGotoSlide',screenNumber+1);
+        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+      })
+      .mouseleave(function(event) {
+        $('#mnheader').slideUp(100);
+        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+      });
   });
 })();
